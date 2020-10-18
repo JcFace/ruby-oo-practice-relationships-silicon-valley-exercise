77a82be7f5503960bc5f6b5eae7387ad6644f965
JcFace <jcv@gmail.com>
2020-10-18 00:24:16 -0700

    Almost there

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,6 +1,7 @@
 class VentureCapitalist
 
     attr_reader :name
+    attr_accessor :total_worth
 
     @@all = []
 
@@ -8,10 +9,14 @@ class VentureCapitalist
         @@all
     end
 
+    def self.tres_commas_club
+        self.all.select {|c| c.total_worth > 1000000000}
+    end
+
     def initialize(name, total_worth)
         @name = name
         @total_worth = total_worth
         @@all << self
-    end
+    end 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,8 @@ end
 techie = Startup.new("Techie", "Jason Techie", "Whatev.com")
 defcon = Startup.new("Defcon", "Jason Techie", "Super.com")
 techie.pivot("Yes,Please.com", "Yaeaah!")
+may = VentureCapitalist.new("May Valencia", 10000000000)
+jesse = VentureCapitalist.new("Jesse Vaughn", 1000000)
 Startup.domains
 
 
