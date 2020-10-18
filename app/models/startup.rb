54a4c2220237c926d64e34b785ca38c7d123a855
JcFace <jcv@gmail.com>
2020-10-17 23:29:05 -0700

    First commit of many

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,20 @@
 class Startup
 
+    attr_reader :founder
+    attr_accessor :name, :domain
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,7 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+techie = Startup.new("Techie", "Jason Techie", "Whatev.com")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
